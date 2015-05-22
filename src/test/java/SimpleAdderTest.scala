3ff0c79ba16ecd7bd89e14ab97da04b795c692db
gsriram7@gmail.com <vaishnavi7>
2015-05-22 19:38:21 +0530

    Selvaram : Added gitignore

--- src/test/java/SimpleAdderTest.scala
+++ src/test/java/SimpleAdderTest.scala
@@ -4,5 +4,8 @@ class SimpleAdderTest extends FunSuite {
   test("Test addition"){
     assert(new SimpleAdder().add(5, 2) == 7)
   }
+  test("Test addition fails"){
+    assert(new SimpleAdder().add(5, 2) == 9)
+  }
 
 }
